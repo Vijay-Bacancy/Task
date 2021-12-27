6d870237322e87757ef12c9fb1f8da0fc91b828a
vd-505 <vijay.daraji@bacancy.com>
2021-12-27 14:02:07 +0530

    init

---
+++ registration-api/src/test/java/com/bacancy/registrationapi/RegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bacancy.registrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
